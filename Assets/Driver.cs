061c109342bf4eed30a2b4aa2433c8e96600cee0
Hugo Rodrigues <86299703+hugordsp@users.noreply.github.com>
2024-05-01 10:57:18 -0300

    Making a capsule move

---
+++ Assets/Driver.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    void Start()
+    {
+        // transform.Rotate(0, 0, 45);
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, 0.1f); //
+        transform.Translate(0, 0.01f, 0);
+
+    }
+}
